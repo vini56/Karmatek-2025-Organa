45da0a8a09af3d7a8d5a8d4a71aec97dd02a5784
Subhradeep1708 <subhradeep1708@gmail.com>
2025-02-09 13:30:00 +0530

    ai integration for organ match

--- ws/src/ai.js
+++ ws/src/ai.js
@@ -1,54 +1,7 @@
-import express, { json } from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
-import cors from "cors";
-import { GoogleGenerativeAI } from "@google/generative-ai";
-
-
-const app = express();
-app.use(cors());
-app.use(json()); 
-
-const server = createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: "*", 
-        methods: ["GET", "POST"],
-    },
-});
-
-io.on("connection", (socket) => {
-    console.log("A user connected");
-
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
-    });
-});
-
-
-app.post("/send-notification", (req, res) => {
-    const { message } = req.body;
-
-    if (!message) {
-        return res.status(400).json({ error: "Message is required" });
-    }
-
-    io.emit("update_matches", { message }); // Broadcast to all connected clients
-    res.json({ success: true, message: "Notification sent" });
-});
-
-server.listen(4000, () => {
-    console.log("Server running on port 4000");
-
-});
 
 
 
-const GEMINI_API_KEY = "AIzaSyDNmJDzrdHX0pAoXT6p0Zc507yPFlbQjwQ";
-const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-app.post("/ai", async (req, res) => {
+const analyseMatch = async (req, res) => {
     const patient = req.body.patient;
     const organ = req.body.organ;
     const match = req.body.match;
@@ -105,7 +58,6 @@ Generate the insights based on the data provided above to ensure the best possib
 
     `;
     const result = await model.generateContent(prompt);
-    return res.json({ data:result.response.candidates[0].content.parts[0].text });
-    
-});
+    return res.json({ data: result.response.candidates[0].content.parts[0].text });
 
+};
